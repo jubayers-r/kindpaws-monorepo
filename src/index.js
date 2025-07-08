0b6ea6ad9f23f14c7f40c06c2628712416da405a
Jubayer Shikder <validjubayer@gmail.com>
2025-07-08 20:40:17 +0600

    Add Express Setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import "dotenv/config"
-import connectDB from "./db/index.js"
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
+const port = process.env.PORT || 8000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    const server = app.listen(port, () => {
+      console.log(`server is running at port : ${port}`);
+    });
+
+    server.on("error", (error) => {
+      console.log("server error ", error);
+      throw error;
+    });
+  })
+
+  .catch((err) => {
+    console.log("MongoDB Connection Failed ", err);
+  });
