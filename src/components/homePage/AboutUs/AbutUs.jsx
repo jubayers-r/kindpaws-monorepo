82e2db0256a69f9a654c9e7ef74780ef226ac170
Jubayer Shikder <validjubayer@gmail.com>
2025-07-16 17:47:38 +0600

    Add AboutUs Section

---
+++ src/components/homePage/AboutUs/AbutUs.jsx
@@ -0,0 +1,110 @@
+import { PawPrint, HeartHandshake, ShieldCheck, Users } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import dog from "@/assets/about-img-dog.png";
+
+const features = [
+  {
+    title: "Easy Pet Discovery",
+    icon: <PawPrint className="w-8 h-8 text-primary" />,
+    description:
+      "Medicenter offers comprehensive dental care for both adults and children from our office at Toronto.",
+  },
+  {
+    title: "Verified Adoptions",
+    icon: <HeartHandshake className="w-8 h-8 text-primary" />,
+    description:
+      "Only real users, real pets, and real adoption requests — all managed with verified accounts and secure data.",
+  },
+  {
+    title: "Built for the Community",
+    icon: <Users className="w-8 h-8 text-primary" />,
+    description:
+      "Whether you’re looking to adopt, donate, or help manage campaigns — this platform is designed for you.",
+  },
+  {
+    title: "Security & Privacy",
+    icon: <ShieldCheck className="w-8 h-8 text-primary" />,
+    description:
+      "Your personal information and adoption data are protected through role-based access and encrypted tokens.",
+  },
+];
+
+const AboutUs = () => {
+  return (
+    <section className="bg-[#f9f5ef] py-16">
+      <div className="w-[95%] max-w-7xl mx-auto text-center">
+        {/* Title */}
+        <div className="mb-8 ">
+          <div className="flex items-center mb-2 gap-2 bg-white rounded-full w-fit px-5 py-1 mx-auto">
+            <PawPrint className="text-primary" />
+            <span className="uppercase text-sm font-semibold text-primary">
+              Who we are
+            </span>
+          </div>
+          <h2 className="text-3xl/tight sm:text-5xl/tight font-semibold text-secondary  sm:max-w-xl text-center mx-auto">
+            Connecting Loving Homes with Pets in Need
+          </h2>
+        </div>
+
+        {/* Main content */}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 items-center">
+          {/* Left 2 features */}
+          <div className="space-y-8">
+            {features.slice(0, 2).map((item, idx) => (
+              <Card
+                key={idx}
+                className="bg-transparent shadow-none text-left border-none"
+              >
+                <CardContent className="flex gap-4 items-start p-0">
+                  <div className="bg-primary/10 p-3 rounded-full">
+                    {item.icon}
+                  </div>
+
+                  <div>
+                    <h3 className="  sm:text-2xl font-semibold pb-2 sm:pb-3">
+                      {item.title}
+                    </h3>
+                    <p className="text-muted-foreground ">{item.description}</p>
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+
+          {/* Center dog image */}
+          <div className="">
+            <img
+              src={dog}
+              alt="Golden Retriever"
+              className="w-full max-w-xs mx-auto"
+            />
+          </div>
+
+          {/* Right 2 features */}
+          <div className="space-y-8">
+            {features.slice(2).map((item, idx) => (
+              <Card
+                key={idx}
+                className="bg-transparent shadow-none text-left border-none"
+              >
+                <CardContent className="flex gap-4 items-start p-0">
+                  <div className="bg-primary/10 p-3 rounded-full">
+                    {item.icon}
+                  </div>
+                  <div>
+                    <h3 className=" sm:text-2xl font-semibold pb-2 sm:pb-3">
+                      {item.title}
+                    </h3>
+                    <p className="text-muted-foreground">{item.description}</p>
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "@/components/homePage/AboutUs/AbutUs";
 import Categories from "@/components/homePage/Categories/Categories";
 import CTA from "@/components/homePage/CTA/CTA";
 import FAQ from "@/components/homePage/FAQ/FAQ";
@@ -11,8 +12,9 @@ const Home = () => {
     <div className="sm:space-y-20 space-y-10 ">
       <Categories />
       <CTA />
+      <AboutUs />
       <Testimonials />
-      <Newsletter/>
+      <Newsletter />
       <FAQ />
     </div>
   );
