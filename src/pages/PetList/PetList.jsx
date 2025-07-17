7f4f8564d7f68ca1b42fb8753ae8b6e626d2f827
Jubayer Shikder <validjubayer@gmail.com>
2025-07-17 11:51:59 +0600

    Add Skeleton to the game

--- src/pages/PetList/PetList.jsx
+++ src/pages/PetList/PetList.jsx
@@ -1,17 +1,22 @@
-import CardComponent from "@/components/ui/SharedComponents/CardComponent/CardComponent";
+import CardComponent from "@/components/shared/CardComponent/CardComponent";
+import SkeletonCardComponent from "@/components/shared/CardComponent/SkeletonCardComponent";
 import { PetData } from "@/data/PetData";
 import React from "react";
 
 const PetList = () => {
   return (
-    <div >
-      <h1 className="text-5xl font-semibold text-center sm:mb-20 mb-10">Pet Listing</h1>
-      <div className="grid grid-cols-5 gap-5">
-        {PetData.map((pet) => (
-          <CardComponent pet={pet} />
-        ))}
+    <>
+      <h1 className="text-5xl font-semibold text-center sm:mb-20 mb-10 ">
+        Pet Listing
+      </h1>
+      <div className="grid 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
+        {PetData ? (
+          PetData.map((pet) => <CardComponent pet={pet} />)
+        ) : (
+          <SkeletonCardComponent />
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
