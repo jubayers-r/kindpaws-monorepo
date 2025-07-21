2198ec25e81ba6821680bbe98d3a384c43b305f7
Jubayer Shikder <validjubayer@gmail.com>
2025-07-22 05:01:13 +0600

    feat: add user model and users db

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    uid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      trim: true,
+     
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import User from "../models/User.js";
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+export default router;
