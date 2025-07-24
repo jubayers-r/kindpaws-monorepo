5061c4aeb046ec2b24f3ff8750e8e41a56bdd129
Jubayer Shikder <validjubayer@gmail.com>
2025-07-24 19:56:20 +0600

    fix: remove added date

--- src/models/Pet.js
+++ src/models/Pet.js
@@ -1,41 +1,40 @@
 import mongoose from "mongoose";
 
-const petSchema = new mongoose.Schema({
-  image: String,
-  name: String,
-  age: String,
-  gender: {
-    type: String,
-    enum: ["Male", "Female", "Unknown"],
-  },
-  location: String,
-  category: {
-    type: String,
-    enum: ["Dog", "Cat", "Bird", "Other"],
-  },
-  shortDescription: String,
-  longDescription: String,
-  tags: [String],
-  dateAdded: {
-    type: Date,
-    default: Date.now,
-  },
-  isFeatured: Boolean,
-  isAdopted: Boolean,
-
-  // 🔗 Reference the owner
-  ownerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-
-  // 🔗 Reference multiple adoption requests
-  adoptionRequests: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "AdoptionRequest",
+const petSchema = new mongoose.Schema(
+  {
+    image: String,
+    name: String,
+    age: String,
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Unknown"],
     },
-  ],
-});
+    location: String,
+    category: {
+      type: String,
+      enum: ["Dog", "Cat", "Bird", "Other"],
+    },
+    shortDescription: String,
+    longDescription: String,
+    tags: [String],
+    isFeatured: Boolean,
+    isAdopted: Boolean,
+
+    // 🔗 Reference the owner
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    // 🔗 Reference multiple adoption requests
+    adoptionRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "AdoptionRequest",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Pet", petSchema);
