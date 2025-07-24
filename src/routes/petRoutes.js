0a1c2663baaba674ecc4d958a6c59790f55d3e4c
Jubayer Shikder <validjubayer@gmail.com>
2025-07-24 16:10:17 +0600

    feat: add patch adoption status toggle & delete pet by petId

--- src/routes/petRoutes.js
+++ src/routes/petRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import Pet from "../models/Pet.js";
-
+import { error, notFound, success } from "../utils/responseUtils.js";
 
 export const router = express.Router();
 
+
+// get all users
 router.get("/", async (req, res) => {
   try {
     const pets = await Pet.find();
@@ -13,5 +15,41 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: "Server Error: Unable to fetch pets" });
   }
 });
+
+// pet adoption status toggle by admin
+router.patch("/statusToggle", async (req, res) => {
+  try {
+    const id = req.query.id;
+
+    const toggle = await Pet.findOneAndUpdate(
+      { _id: id },
+      [{ $set: { isAdopted: { $not: "$isAdopted" } } }],
+      { new: true }
+    );
+    if (!toggle) {
+      return notFound(res, "Pet");
+    }
+    success(res, toggle);
+  } catch (err) {
+    error(res, err);
+  }
+});
+
+// pet delete
+router.delete("/delete", async (req, res) => {
+  try {
+    const petId = req.query.id;
+
+    const deletePet = await Pet.findByIdAndDelete(petId);
+    if (!deletePet) {
+      return notFound(res, "Pet");
+    }
+    success(res, deletePet);
+  } catch (err) {
+    error(res, err);
+  }
+});
+
+
 
 export default router;
