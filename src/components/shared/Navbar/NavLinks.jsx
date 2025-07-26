fd800d9c52a68b9d78e2093cd2926c33418575ba
Jubayer Shikder <validjubayer@gmail.com>
2025-07-26 17:25:27 +0600

    style: change order of dashboard nav

--- src/components/shared/Navbar/NavLinks.jsx
+++ src/components/shared/Navbar/NavLinks.jsx
@@ -8,28 +8,24 @@ const NavLinks = () => {
 
   return (
     <ul className="flex gap-2">
-      {user && (
-        <li className="hover:text-amber-500">
-          <NavLink to="dashboard">Dashboard</NavLink>
-          <span
-            aria-hidden="true"
-            className="inline-block h-1.5 w-1.5 rounded-full bg-orange-400 lg:mx-4"
-          />
-        </li>
-      )}
       {NavItems.map((item, i) => (
         <li key={i} className="hover:text-amber-500">
           {" "}
           <NavLink to={item.href}>{item.label}</NavLink>
           {/* Render dot except after last item */}
-          {i !== NavItems.length - 1 && (
+          {
             <span
               aria-hidden="true"
               className="inline-block h-1.5 w-1.5 rounded-full bg-orange-400 lg:mx-4"
             />
-          )}
+          }
         </li>
       ))}
+      {user && (
+        <li className="hover:text-amber-500">
+          <NavLink to="dashboard">Dashboard</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
