c27c1d54f1072161cbfd92fdf1efb6e6ac7868a2
Jubayer Shikder <validjubayer@gmail.com>
2025-08-17 11:01:36 +0600

    chore: remove auth navitems while logged out

--- client/src/components/shared/Navbar/NavLinks.jsx
+++ client/src/components/shared/Navbar/NavLinks.jsx
@@ -1,20 +1,26 @@
 import { NavLink } from "react-router";
 import { NavItems } from "../../../data/NavItems";
 import { useAuth } from "@/hooks/useAuth";
-import { AuthContext } from "@/context/auth/AuthContext";
 
 const NavLinks = () => {
   const { user } = useAuth();
 
+  // make a copy instead of mutating NavItems
+  const items = [...NavItems];
+
+  if (user) {
+    items.push(
+      { label: "Dashboard", href: "/dashboard" },
+      { label: "Contact Us", href: "/contact-us" }
+    );
+  }
+
   return (
     <ul className="flex gap-2">
-     
-      {NavItems.map((item, i) => (
+      {items.map((item, i) => (
         <li key={i} className="hover:text-amber-500">
-          {" "}
           <NavLink to={item.href}>{item.label}</NavLink>
-          {/* Render dot except after last item */}
-          {i !== NavItems.length - 1 && (
+          {i !== items.length - 1 && (
             <span
               aria-hidden="true"
               className="inline-block h-1.5 w-1.5 rounded-full bg-orange-400 lg:mx-4"
--- client/src/data/NavItems.jsx
+++ client/src/data/NavItems.jsx
@@ -2,6 +2,5 @@ export const NavItems = [
   { label: "Home", href: "/" },
   { label: "Adopt", href: "/adopt" },
   { label: "Campaigns", href: "/campaigns" },
-  { label: "Dashboard", href: "/dashboard" },
-  { label: "Contact Us", href: "/contact-us" },
+
 ];
